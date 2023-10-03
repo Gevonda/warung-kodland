48a0881e248224e3d1e40b971444c8b535d3d767
Gevonda <131878888+Gevonda@users.noreply.github.com>
2023-10-03 17:19:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
+
+function auth(){
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if(email== "admin@gmail.com" && password=="admin123"){
+        window.location.assign("name.html");
+        alert("login succesfully");
+    }
+    else{
+        alert("invalid information");
+        return;
+    }
+}
